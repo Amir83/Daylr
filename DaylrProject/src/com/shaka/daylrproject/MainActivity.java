5ae143d0bc0ce4e7a5cba0ffee23bf01ef2fa51b
Kash <khashy.rastin@gmail.com>
2012-11-05 20:07:47 +0100

    test

--- DaylrProject/src/com/shaka/daylrproject/MainActivity.java
+++ DaylrProject/src/com/shaka/daylrproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 	/* shaho This is IT WHAT */
+	/* Kash */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
