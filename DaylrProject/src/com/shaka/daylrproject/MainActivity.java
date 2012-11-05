5d4a226f6fae3f25a71abbc9b4493f5277cc79b5
shaho <hola@192.168.0.14>
2012-11-05 19:55:28 +0100

    Första i er mun

--- DaylrProject/src/com/shaka/daylrproject/MainActivity.java
+++ DaylrProject/src/com/shaka/daylrproject/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	/* shaho */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
